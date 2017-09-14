c848808eb09f1617c140e84c92a440eaea9d5d1c
simon.huang <simon.huang@nogle.com>
2017-09-14 18:31:43 +0800

    to be fixed: exit room

--- src/main/java/prj/serenasimon/cache/ChatCache.java
+++ src/main/java/prj/serenasimon/cache/ChatCache.java
@@ -1,23 +1,14 @@
 package prj.serenasimon.cache;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.UUID;
-import java.util.stream.Collectors;
 
 import javax.websocket.Session;
 
-import prj.serenasimon.socket.ChatClient;
-import prj.serenasimon.socket.ChatServer;
-import prj.serenasimon.socket.Conversation;
-
 public class ChatCache {
 
-    private static volatile Map<UUID, ChatServer> chatServer;
-    private static volatile Map<UUID, ChatClient> chatClient;
-    private static volatile Map<UUID, Conversation> conversations;
     private static volatile Map<String, List<Session>> chatSessions;
 
     static {
@@ -25,47 +16,7 @@ public class ChatCache {
     }
 
     private static void initCache() {
-        setChatServer(new HashMap<UUID, ChatServer>());
-        setChatClient(new HashMap<UUID, ChatClient>());
-        setConversations(new HashMap<UUID, Conversation>());
         setChatSessions(new HashMap<String, List<Session>>());
-    }
-
-    public static Map<UUID, ChatServer> getChatServer() {
-        return chatServer;
-    }
-
-    public static void setChatServer(Map<UUID, ChatServer> chatServer) {
-        ChatCache.chatServer = chatServer;
-    }
-
-    public static Map<UUID, ChatClient> getChatClient() {
-        return chatClient;
-    }
-
-    public static void setChatClient(Map<UUID, ChatClient> chatClient) {
-        ChatCache.chatClient = chatClient;
-    }
-
-    public static Map<UUID, Conversation> getConversations() {
-        return conversations;
-    }
-
-    public static void setConversations(Map<UUID, Conversation> conversations) {
-        ChatCache.conversations = conversations;
-    }
-
-    public static ArrayList<ChatServer> getServerByUserID(String userid) {
-        return (ArrayList<ChatServer>) chatServer.values().stream()
-            .filter(s -> s.getParticipants().contains(userid))
-            .collect(Collectors.toList());
-    }
-
-    public static ArrayList<ChatClient> getClientByUserID(String userid) {
-        return (ArrayList<ChatClient>) chatClient.values().stream()
-            .filter(s -> s.getParticipant().contains(userid))
-            .collect(Collectors.toList());
-
     }
 
     public static Map<String, List<Session>> getChatSessions() {
@@ -74,5 +25,19 @@ public class ChatCache {
 
     public static void setChatSessions(Map<String, List<Session>> chatSessions) {
         ChatCache.chatSessions = chatSessions;
+    }
+
+    public static void addChatSessions(Session chatSession) {
+        if (chatSessions.get(chatSession.getUserProperties().get("room")) == null) {
+            chatSessions.put((String) chatSession.getUserProperties().get("room"), Arrays.asList(chatSession));
+        } else {
+            chatSessions.get(chatSession.getUserProperties().get("room")).add(chatSession);
+        }
+    }
+
+    public static void removeChatSessions(Session chatSession) {
+        if (chatSessions.get(chatSession.getUserProperties().get("room")) != null) {
+            chatSessions.get(chatSession.getUserProperties().get("room")).remove(chatSession);
+        }
     }
 }
--- src/main/java/prj/serenasimon/websocket/ChatEndpoint.java
+++ src/main/java/prj/serenasimon/websocket/ChatEndpoint.java
@@ -1,53 +1,50 @@
 package prj.serenasimon.websocket;
 
 import java.io.IOException;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
 
 import javax.websocket.EncodeException;
 import javax.websocket.OnClose;
 import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
+import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import prj.serenasimon.cache.ChatCache;
 import prj.serenasimon.chat.ChatMessageDecoder;
 import prj.serenasimon.chat.ChatMessageEncoder;
 import prj.serenasimon.datas.ChatMessage;
 
-@ServerEndpoint(value = "/chat", encoders = ChatMessageEncoder.class, decoders = ChatMessageDecoder.class)
+@ServerEndpoint(value = "/chat/{room}", encoders = ChatMessageEncoder.class, decoders = ChatMessageDecoder.class)
 public class ChatEndpoint {
     private static final Logger logger = LogManager.getLogger(ChatEndpoint.class);
 
-    private static Set<Session> clients = Collections.synchronizedSet(new HashSet<Session>());
-
     @OnOpen
-    public void open(final Session session) {
-        logger.info("ws opened; session: {}", session.getId());
-        clients.add(session);
+    public void open(Session session, @PathParam("room") final String room) {
+        logger.info("ws opened; room: {}", room);
+
+        session.getUserProperties().put("room", room);
+        ChatCache.addChatSessions(session);
     }
 
     @OnClose
     public void close(final Session session) {
-        logger.info("ws closed; session: {}", session.getId());
-        clients.remove(session);
+        logger.info("ws closed; room: {}", session.getUserProperties().get("room"));
+        ChatCache.removeChatSessions(session);
     }
 
     @OnMessage
     public void onMessage(final Session session, final ChatMessage chatMessage) {
-        synchronized (clients) {
-            for (Session client : clients) {
-                if (!client.equals(session)) {
-                    try {
-                        client.getBasicRemote().sendObject(chatMessage);
-                    } catch (IOException | EncodeException e) {
-                        // TODO Auto-generated catch block
-                        e.printStackTrace();
-                    }
+        for (Session client : ChatCache.getChatSessions().get(session.getUserProperties().get("room"))) {
+            if (!client.equals(session)) {
+                try {
+                    client.getBasicRemote().sendObject(chatMessage);
+                } catch (IOException | EncodeException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
                 }
             }
         }
